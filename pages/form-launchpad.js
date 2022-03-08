daa141f8a14419d165476cf34a9ffae26c065660
James <kingjamesegun@gmail.com>
2022-03-08 21:19:34 +0100

    Console removed

--- pages/form-launchpad.js
+++ pages/form-launchpad.js
@@ -208,7 +208,6 @@ const LaunchpadForm = () => {
 								<CountryDropdown
 									className='block w-full bg-gray-200 text-gray-700 border border-gray-100 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white'
 									onChange={(val) => {
-										console.log(val);
 										setCountry(val);
 									}}
 									value={country}
