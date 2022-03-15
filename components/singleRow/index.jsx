eedd2c411b24730aa2af261cb9d5dcf37dac63dd
Usman Asif <82535372+UsmanAsif193@users.noreply.github.com>
2022-03-15 21:09:18 +0500

    feat: slider responsiveness fixed

--- components/singleRow/index.jsx
+++ components/singleRow/index.jsx
@@ -1,71 +1,17 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
-import Slider from "react-slick";
-function NextArrow(props) {
-  const { className, onClick } = props;
-  return (
-    <div
-      className={`${className} before:text-blackImportant`}
-      onClick={onClick}
-    />
-  );
-}
+import SlickSlider from "../slickSlider";
 
-function PrevArrow(props) {
-  const { className, onClick } = props;
-  return (
-    <div
-      className={`${className} before:text-blackImportant`}
-      onClick={onClick}
-    />
-  );
-}
 const SingleRow = ({ data, renderItem }) => {
-  const options = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    arrows: true,
-    nextArrow: <NextArrow />,
-    prevArrow: <PrevArrow />,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
   return (
     <div className="">
-      <Slider {...options}>
+      <SlickSlider>
         {data.map((item, i) => (
           <div key={i} className="px-4">
             {renderItem(item, i)}
           </div>
         ))}
-      </Slider>
+      </SlickSlider>
     </div>
   );
 };
---
+++ components/slickSlider/index.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import Slider from "react-slick";
+function NextArrow(props) {
+  const { className, onClick } = props;
+  return (
+    <div
+      className={`${className} before:text-blackImportant`}
+      onClick={onClick}
+    />
+  );
+}
+
+function PrevArrow(props) {
+  const { className, onClick } = props;
+  return (
+    <div
+      className={`${className} before:text-blackImportant`}
+      onClick={onClick}
+    />
+  );
+}
+const SlickSlider = ({ children }) => {
+  const options = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 3,
+    arrows: true,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 2,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  return <Slider {...options}>{children}</Slider>;
+};
+
+export default SlickSlider;
