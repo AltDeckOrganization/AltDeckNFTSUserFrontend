790a1f56506d3f6c4cf8eb45f20b5744a02b4aa6
Usman Asif <82535372+UsmanAsif193@users.noreply.github.com>
2022-03-18 22:07:29 +0500

    feat: privacy policy page updated

--- components/tos/TosPara.js
+++ components/tos/TosPara.js
@@ -4,7 +4,7 @@ function TosPara({ title, desc, titleNumber }) {
   return (
     <div>
       <p className="text-base text-black text-sm font-bold">
-        <span className="text-[#50C9C3]">{titleNumber}. </span>
+        {titleNumber && <span className="text-[#50C9C3]">{titleNumber}. </span>}
         {title}
       </p>
       <p className="leading-loose leading-6 mb-5">{desc}</p>
--- pages/privacy.js
+++ pages/privacy.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import PrivacyPolicyDetails from '../components/privacyPolicyDetails';
+import React from "react";
+import PrivacyPolicyDetails from "../components/privacyPolicyDetails";
 
 const PrivacyPolicy = () => {
-	return (
-		<div className='xl:w-[1156px] mx-auto mt-20 lg:w-[900px] w-full'>
-			<div className='px-5 lg:px-0'>
-				<h1 className='text-[#50C9C3] text-2xl md:text-3xl'>Privacy Policy</h1>
+  return (
+    <div className="xl:w-[1156px] mx-auto mt-20 lg:w-[900px] w-full">
+      <div className="px-5 lg:px-0 pt-0 md:pt-5">
+        <h1 className="text-3xl text-black font-semibold my-5 md:text-3xl">
+          Privacy Policy
+        </h1>
 
-				<p className='text-sm text-[#808080] mb-5'>
-					Privacy Policy(Version 1.0)
-				</p>
-			</div>
-			<PrivacyPolicyDetails />
-		</div>
-	);
+        <p className="text-sm text-[#808080] mb-5">
+          Privacy Policy(Version 1.0)
+        </p>
+      </div>
+      <PrivacyPolicyDetails />
+    </div>
+  );
 };
 
 export default PrivacyPolicy;
