fdb344f7a589eea1d2238615b444624ec40b803b
Usman Asif <82535372+UsmanAsif193@users.noreply.github.com>
2022-03-18 22:04:26 +0500

    tos updated

--- components/tos/TosPara.js
+++ components/tos/TosPara.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React from "react";
 
-function TosPara({title, desc}) {
-	return (
-		<div>
-			<p className='text-base text-black text-sm font-bold'>{title}</p>
-			<p className='leading-loose leading-6 mb-5'>
-                {desc}
-			</p>
-		</div>
-	);
+function TosPara({ title, desc, titleNumber }) {
+  return (
+    <div>
+      <p className="text-base text-black text-sm font-bold">
+        <span className="text-[#50C9C3]">{titleNumber}. </span>
+        {title}
+      </p>
+      <p className="leading-loose leading-6 mb-5">{desc}</p>
+    </div>
+  );
 }
 
 export default TosPara;
