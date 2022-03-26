f399c5ef7605cbe9505bffbb7b2caca00c8fb658
Usman Asif <82535372+UsmanAsif193@users.noreply.github.com>
2022-03-27 00:04:06 +0500

    feat: drop page text and table updated

--- pages/drops.js
+++ pages/drops.js
@@ -5,14 +5,11 @@ import DropsTable from "../components/dropsTable";
 const Drops = () => {
   return (
     <div className="xl:w-[1156px] mx-auto lg:w-[900px] w-full mt-28 px-4 overflow-hidden md:px-0">
-      <h1 className="text-3xl">Drops</h1>
-      <p className="py-4">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis,
-        quaerat? Molestiae eligendi odio, eos placeat praesentium, porro
-        repellat quis, assumenda vero architecto exercitationem dolorum! Quis
-        quam deleniti laboriosam, quibusdam distinctio assumenda eos! Assumenda
-        hic quas fugiat quaerat adipisci, dolorem porro magnam amet nostrum nemo
-        dolore, dignissimos eveniet, ad repudiandae alias?
+      <h1 className="text-3xl text-center">Drops</h1>
+      <p className="py-4 text-center">
+        Projects listed are not an endorsement or a guarantee of a listing on
+        AltDeck <br />
+        Apply here as an upcoming drop - Drop Application
       </p>
       {dropData.map((item, i) => (
         <div key={i} className={`${i !== 0 ? "mt-20" : "mt-8"}`}>
