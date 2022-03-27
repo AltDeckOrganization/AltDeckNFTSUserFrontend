fb1bf6d710bd65ea5a30072e3e8b293a6fd7a44e
Usman Asif <82535372+UsmanAsif193@users.noreply.github.com>
2022-03-28 00:06:40 +0500

    feat: navbar link fixed

--- components/navbar/index.jsx
+++ components/navbar/index.jsx
@@ -116,7 +116,7 @@ export default function Example() {
               link1="Apply for launchpad"
               apply="form-launchpad"
               link2="Apply for drop"
-              apply2="drop-form"
+              apply2="drops-form"
             />
           </li>
 
@@ -236,7 +236,7 @@ export default function Example() {
                   // apply="form-listing"
                   apply="form-launchpad"
                   link2="Apply for drop"
-                  apply2="drop-form"
+                  apply2="drops-form"
                   onClick={handleMenuClick}
                 />
               </li>
