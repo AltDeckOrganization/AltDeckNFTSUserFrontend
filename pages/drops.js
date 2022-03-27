e9795f6ae8f3b0bd64b6cecdf41defe1773df8ca
Usman Asif <82535372+UsmanAsif193@users.noreply.github.com>
2022-03-28 00:08:34 +0500

    feat: seo component added

--- pages/drops.js
+++ pages/drops.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { dropData } from "../components/data/dropsData";
 import DropsTable from "../components/dropsTable";
+import SEO from "../components/seo/SEO";
 
 const Drops = () => {
   return (
     <div className="xl:w-[1156px] mx-auto lg:w-[900px] w-full mt-28 px-4 overflow-hidden md:px-0">
+      <SEO />
       <h1 className="text-3xl text-center">Drops</h1>
       <p className="py-4 text-center">
         Projects listed are not an endorsement or a guarantee of a listing on
