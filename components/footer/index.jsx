282a3194bbaacd5f3c854a71982fd77282ff8a46
Usman Asif <82535372+UsmanAsif193@users.noreply.github.com>
2022-03-30 21:53:35 +0500

    feat: footer dark mode added

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -1,19 +1,31 @@
 import Link from "next/link";
 import React from "react";
+import { useDarkMode } from "../../context/darkMode";
 import footerStyles from "./Footer.module.css";
 
 function Footer() {
+  const { darkMode } = useDarkMode();
   return (
-    <div className={`${footerStyles.footer} flex items-center`}>
+    <div
+      className={`${footerStyles.footer} flex items-center ${
+        darkMode && "bg-black text-white"
+      }`}
+    >
       <div className="">
         &copy; 2021{" "}
-        <span className={`text-black font-bold text-sm  ${footerStyles.span}`}>
+        <span
+          className={`text-black font-bold text-sm  ${footerStyles.span} ${
+            darkMode && "text-whiteImportant"
+          }`}
+        >
           AltDeck
         </span>
       </div>
       <></>
       <div>
-        <ul className={`flex space-x-8 text-[#808080] text-sm font-bold ${footerStyles.footerUl}`}>
+        <ul
+          className={`flex space-x-8 text-[#808080] text-sm font-bold ${footerStyles.footerUl}`}
+        >
           <li>
             <Link href="/tos">TOS</Link>
           </li>
