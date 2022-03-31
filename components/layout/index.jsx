edd6752c4adf17b3d885fcdcb35a2b0c7963d191
Usman Asif <82535372+UsmanAsif193@users.noreply.github.com>
2022-03-31 19:53:09 +0500

    fix: layout dark mode fixed

--- components/layout/index.jsx
+++ components/layout/index.jsx
@@ -5,10 +5,10 @@ import { useDarkMode } from "../../context/darkMode";
 export default function Layout({ children }) {
   const { darkMode } = useDarkMode();
   return (
-    <>
+    <div className={`min-h-screen ${darkMode && "bg-black"}`}>
       <Nav />
       <main className={darkMode && "bg-black text-white"}>{children}</main>
       <Footer />
-    </>
+    </div>
   );
 }
