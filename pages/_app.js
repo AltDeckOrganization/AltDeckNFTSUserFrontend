51929b1654e54c79d74e1e9447b3c25fc5d75292
Usman Asif <82535372+UsmanAsif193@users.noreply.github.com>
2022-04-02 00:56:22 +0500

    feat: ga fixed

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import Script from "next/script";
 
 import { useRouter } from "next/router";
-import * as ga from "next/router";
+import * as ga from "../lib/ga";
 
 //components
 import Layout from "../components/layout";
