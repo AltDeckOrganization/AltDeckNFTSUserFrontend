e9e243dcd5f53e5489107af01ce2c52bcb2d07a3
Usman Asif <82535372+UsmanAsif193@users.noreply.github.com>
2022-04-02 23:12:37 +0500

    feat: collection page dark mode added

--- components/select/select.js
+++ components/select/select.js
@@ -1,18 +1,32 @@
+import { useDarkMode } from "../../context/darkMode";
 
-function select() {
-    return (
-        <div>    
-            <div className='my-2 text-sm text-gray-500 uppercase'>Background</div>
-            <div className="mb-3 select ">
-                <select className="form-select form-select-sm  block w-full px-2 py-3 text-sm font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none " aria-label=".form-select-sm example">
-                <option selected>Background</option>
-                <option value="1">Base</option>
-                <option value="2">Blue</option>
-                <option value="3"></option>
-                </select>
-            </div>
-        </div>
-    )
+function Select() {
+  const { darkMode } = useDarkMode();
+  return (
+    <div>
+      <div
+        className={`my-2 text-sm text-gray-500 uppercase ${
+          darkMode ? "text-gray-100" : "text-gray-500"
+        }`}
+      >
+        Background
+      </div>
+      <div className="mb-3 select ">
+        <select
+          className={`form-select form-select-sm block w-full px-2 py-3 text-sm font-normal bg-clip-padding bg-no-repeat border border-solid rounded transition ease-in-out m-0 focus:outline-none ${
+            darkMode
+              ? "text-gray-100 bg-black border-white focus:text-gray-100 focus:bg-black"
+              : "text-gray-700 bg-white border-gray-300 focus:text-gray-700 focus:bg-white"
+          }`}
+          aria-label=".form-select-sm example"
+        >
+          <option selected>Background</option>
+          <option value="1">Base</option>
+          <option value="2">Blue</option>
+        </select>
+      </div>
+    </div>
+  );
 }
 
-export default select
+export default Select;
