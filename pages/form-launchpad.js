d5e9819a90990c4ae3c0d5b0fceeaf7ab9a3109f
Usman Asif <82535372+UsmanAsif193@users.noreply.github.com>
2022-04-12 01:36:37 +0500

    feat: form-launchpad updated for logined

--- pages/form-launchpad.js
+++ pages/form-launchpad.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState, useEffect } from "react";
+import React, { Fragment, useState, useEffect, useMemo } from "react";
 import Form from "../components/forms";
 import {
   CountryDropdown,
@@ -15,6 +15,8 @@ import DropdownForm from "../components/dropdown/DropdownForm";
 import Link from "next/link";
 import ReCAPTCHA from "react-google-recaptcha";
 import { useDarkMode } from "../context/darkMode";
+import { ConnectWallet } from "../components/connectWallet";
+import { useWallet } from "@solana/wallet-adapter-react";
 
 const LaunchpadForm = () => {
   const [name, setName] = useState("");
@@ -132,7 +134,22 @@ const LaunchpadForm = () => {
       });
   };
   const { darkMode } = useDarkMode();
+  const { publicKey } = useWallet();
+  const base58 = useMemo(() => publicKey?.toBase58(), [publicKey]);
 
+  if (!base58) {
+    return (
+      <div className="px-5 md:px-0 xl:w-[1156px] xl:mx-auto lg:px-28 xl:px-0 text-center w-full pt-28">
+        <h1 className="text-[#50C9C3] font-bold mb-5 text-3xl">
+          You are not Logged In.
+        </h1>
+        Please Connect your wallet
+        <div className="flex items-center justify-center">
+          <ConnectWallet className="flex justify-center items-center w-fit border border-[#50C9C3] text-[#50c9c3] shadow-sm px-4 py-2 text-base  w-full  border rounded rounded-md  mt-5  font-medium rounded " />
+        </div>
+      </div>
+    );
+  }
   return (
     <div>
       <div className="launchPad  md:px-0 xl:w-[1156px] xl:mx-auto lg:px-28  w-full pt-20 md:pt-28">
