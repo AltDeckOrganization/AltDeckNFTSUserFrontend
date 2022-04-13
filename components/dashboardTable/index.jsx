2f80c803f319bca1814fcb1a837167ccda0edc17
Usman Asif <82535372+UsmanAsif193@users.noreply.github.com>
2022-04-13 17:13:42 +0500

    feat: side coloums fixed for light and dark mode

---
+++ components/dashboardTable/index.jsx
@@ -0,0 +1,166 @@
+/* eslint-disable @next/next/no-img-element */
+import {
+  TableCell,
+  TableHead,
+  TableRow,
+  Table,
+  TableBody,
+} from "@mui/material";
+import Link from "next/link";
+import { useDarkMode } from "../../context/darkMode";
+
+const DashboardTable = ({ rows, scroll }) => {
+  const { darkMode } = useDarkMode();
+  return (
+    <div
+      className={`w-full mt-5 ${
+        scroll && "overflow-x-scroll md:overflow-hidden"
+      }`}
+    >
+      <Table className="min-w-[750px]" size="small">
+        <CustomTableHead />
+        <TableBody
+          className="bg-white"
+          sx={{
+            "& .MuiTableCell-root": {
+              border: "none",
+            },
+            "& .MuiTableRow-root:hover": {
+              backgroundColor: "rgba(79, 201, 195, 0.20)",
+            },
+          }}
+        >
+          {rows.map((row, index) => (
+            <>
+              <TableRow
+                hover
+                tabIndex={-1}
+                className={`border-b-0 border-l-4 hover:border-l-orange 
+                border-transparent ${
+                  index % 2 === 0
+                    ? darkMode
+                      ? "bg-[#1e8b84] hover:bg-[#6db9b5]"
+                      : "bg-[#50C9C314]/10 hover:bg-[#50C9C314]/20"
+                    : darkMode
+                    ? "bg-black hover:bg-[#6db9b5]"
+                    : "bg-white"
+                } 
+                 cursor-pointer`}
+              >
+                <TableCell>
+                  <div className={`flex flex-row items-center`}>
+                    <img
+                      src={row.image}
+                      alt=""
+                      className={`w-[40px] h-[40px] rounded-sm mr-2`}
+                    />
+                    <p
+                      className={`py-3 text-[16px] font-medium ${
+                        darkMode ? "text-white" : "text-black"
+                      }`}
+                    >
+                      {row.name}
+                    </p>
+                  </div>
+                </TableCell>
+                <TableCell>
+                  <p
+                    className={`text-[16px] ${
+                      darkMode ? "text-white" : "text-black"
+                    }`}
+                  >
+                    {row.price} SOL
+                  </p>
+                </TableCell>
+                <TableCell>
+                  <p
+                    className={`text-[16px] ${
+                      darkMode ? "text-white" : "text-black"
+                    }`}
+                  >
+                    {row.tokens}
+                  </p>
+                </TableCell>
+                <TableCell>
+                  <p
+                    className={`text-[16px] ${
+                      darkMode ? "text-white" : "text-black"
+                    }`}
+                  >
+                    {row.date}
+                  </p>
+                </TableCell>
+                <TableCell>
+                  <p
+                    className={`text-[16px] ${
+                      darkMode ? "text-white" : "text-black"
+                    }`}
+                  >
+                    {row.filled}
+                  </p>
+                </TableCell>
+                <TableCell>
+                  <div
+                    className={`text-[16px] ${
+                      darkMode
+                        ? row.status === "Approved"
+                          ? "text-green-300"
+                          : "text-red-300"
+                        : row.status === "Approved"
+                        ? " text-green-600"
+                        : "text-red-300"
+                    }`}
+                  >
+                    {row.status}
+                  </div>
+                </TableCell>
+              </TableRow>
+            </>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+};
+
+const headCells = [
+  { id: "collection", label: "Collection" },
+  { id: "price", label: "Token Price" },
+  { id: "tokens", label: "Tokens" },
+  { id: "date", label: "Launch Date" },
+  { id: "filled", label: "Filled" },
+  { id: "status", label: "Status" },
+];
+
+const CustomTableHead = () => {
+  const { darkMode } = useDarkMode();
+  return (
+    <TableHead className="bg-transparent">
+      <TableRow
+        sx={{
+          "& .MuiTableCell-root": {
+            border: "none",
+          },
+        }}
+      >
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            className="capitalize text-[12px]"
+            align={headCell.numeric ? "right" : "left"}
+          >
+            <p
+              className={`font-semibold text-[15px] ${
+                darkMode ? "text-white" : "text-black"
+              } uppercase pb-2`}
+            >
+              {headCell.label}
+            </p>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+};
+
+export default DashboardTable;
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,10 +1,14 @@
 import { useWallet } from "@solana/wallet-adapter-react";
 import React, { useMemo, useState } from "react";
 import { ConnectWallet } from "../components/connectWallet";
+import DashboardTable from "../components/dashboardTable";
+import { dashboardData } from "../components/data/dashboardData";
+import { useDarkMode } from "../context/darkMode";
 
 const Dashboard = () => {
   const [value, setValue] = useState(0);
   const { publicKey } = useWallet();
+  const { darkMode } = useDarkMode();
   const base58 = useMemo(() => publicKey?.toBase58(), [publicKey]);
 
   if (!base58) {
@@ -29,26 +33,44 @@ const Dashboard = () => {
         <div className="flex flex-col justify-between">
           <div>
             <div
-              className={`my-2 text-white py-2 px-2 text-center rounded-xl cursor-pointer hover:bg-[#50c9c3] hover:text-black border border-[#50C9C3] ${
-                value === 0 && "text-black bg-[#50c9c3]"
-              }`}
+              className={`my-2 py-2 px-2 text-center rounded-xl cursor-pointer border border-[#50C9C3] ${
+                value === 0
+                  ? darkMode
+                    ? "text-black bg-[#50c9c3] hover:bg-[#50c9c3] hover:text-black"
+                    : "text-white bg-[#50c9c3]"
+                  : darkMode
+                  ? "text-white hover:bg-[#50c9c3] hover:text-black"
+                  : "text-black hover:bg-[#50c9c3] hover:text-white"
+              } `}
               onClick={() => setValue(0)}
             >
               Statistics
             </div>
             <div
-              className={`my-2 text-white py-2 px-2 text-center rounded-xl cursor-pointer hover:bg-[#50c9c3] hover:text-black border border-[#50C9C3] ${
-                value === 1 && "text-black bg-[#50c9c3]"
-              }`}
+              className={`my-2 py-2 px-2 text-center rounded-xl cursor-pointer border border-[#50C9C3] ${
+                value === 1
+                  ? darkMode
+                    ? "text-black bg-[#50c9c3] hover:bg-[#50c9c3] hover:text-black"
+                    : "text-white bg-[#50c9c3]"
+                  : darkMode
+                  ? "text-white hover:bg-[#50c9c3] hover:text-black"
+                  : "text-black hover:bg-[#50c9c3] hover:text-white"
+              } `}
               onClick={() => setValue(1)}
             >
               My Applications
             </div>
           </div>
           <div
-            className={`my-2 text-white py-2 px-2 text-center rounded-xl cursor-pointer hover:bg-[#50c9c3] hover:text-black border border-[#50C9C3] ${
-              value === 2 && "text-black bg-[#50c9c3]"
-            }`}
+            className={`my-2 py-2 px-2 text-center rounded-xl cursor-pointer border border-[#50C9C3] ${
+              value === 2
+                ? darkMode
+                  ? "text-black bg-[#50c9c3] hover:bg-[#50c9c3] hover:text-black"
+                  : "text-white bg-[#50c9c3]"
+                : darkMode
+                ? "text-white hover:bg-[#50c9c3] hover:text-black"
+                : "text-black hover:bg-[#50c9c3] hover:text-white"
+            } `}
             onClick={() => setValue(2)}
           >
             Support
@@ -71,22 +93,7 @@ const Dashboard = () => {
               </div>
             </div>
           )}
-          {value === 1 && (
-            <div className="grid grid-rows-3 grid-flow-col gap-4 h-full">
-              <div className="col-span-2 bg-[#1a1a1a] rounded-xl flex items-center justify-center h-full w-full">
-                02
-              </div>
-              <div className="row-span-2 col-span-2 bg-[#1a1a1a] rounded-xl flex items-center justify-center h-full w-full">
-                03
-              </div>
-              <div className="row-span-2 col-span-2 bg-[#1a1a1a] rounded-xl flex items-center justify-center h-full w-full">
-                03
-              </div>
-              <div className="col-span-2 bg-[#1a1a1a] rounded-xl flex items-center justify-center h-full w-full">
-                02
-              </div>
-            </div>
-          )}
+          {value === 1 && <DashboardTable rows={dashboardData} />}
         </div>
       </div>
     </div>
