9627f65173f4cedf61a33f3e2b3560bc65293077
pwilson77 <p.o.yirenkyi@gmail.com>
2022-04-14 16:30:29 +0000

    test fix for mint page

--- components/data/launchpadData.js
+++ components/data/launchpadData.js
@@ -4,9 +4,9 @@ export const launchpadData = [
     name: "Anime Immortals Village",
     price: "0.8",
     tokens: "2000",
-    date: "Today",
+    date: "Past",
     filled: "17:00 UTC",
-    status: "WL Sale @ 16:00 UTC",
+    status: "Completed",
     link: "mintpage/37",
   },
 ];
