1187c15e9fc659ec739b0375b88cc3a8903fd7ac
Usman Asif <82535372+UsmanAsif193@users.noreply.github.com>
2022-04-15 23:51:55 +0500

    feat: cards link fixed

--- components/data/cardData.js
+++ components/data/cardData.js
@@ -3,7 +3,7 @@ export const upcomingLaunches = [
     head: "DoggoStyle",
     img: "/upcomingLaunches/doggoStyle.jpg",
     para: "DoggoStyle is creating the greatest 3D VR card game ever! 8888 collection",
-    link: "https://www.mintdoggostyle.art/",
+    link: "https://twitter.com/doggostyleart?s=21",
     wlSale: "WL April 14th, 8:30pm UTC",
     publicSale: "Public April 14th, 9pm UTC",
   },
