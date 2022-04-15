39f3408f7345d109dc97fa88370aebd5afa6263e
Usman Asif <82535372+UsmanAsif193@users.noreply.github.com>
2022-04-15 23:51:25 +0500

    cards links updated

--- components/data/cardData.js
+++ components/data/cardData.js
@@ -4,15 +4,25 @@ export const upcomingLaunches = [
     img: "/upcomingLaunches/doggoStyle.jpg",
     para: "DoggoStyle is creating the greatest 3D VR card game ever! 8888 collection",
     link: "https://twitter.com/doggostyleart?s=21",
-    wlSale: "WL April 14th, 8:30pm UTC",
-    publicSale: "Public April 14th, 9pm UTC",
+    wlSale: "WL + Public mint, April 15th, 9pm UTC",
+    publicSale: "",
   },
   {
     head: "Digital Detonators",
     img: "/upcomingLaunches/digitalDetonators.jpg",
     para: "A 100 supply collection with real-world utility coming to Solana with a unique design ",
     link: "https://twitter.com/d_detonators?s=21",
-    wlSale: "WL April 15th, 6pm UTC",
-    publicSale: "Public April 15th, 7pm UTC ",
+    wlSale: "WL: April 17th, 6pm UTC",
+    publicSale: "Public April 17th 7pm UTC",
+  },
+];
+export const lanchpadCards = [
+  {
+    head: "DoggoStyle",
+    img: "/upcomingLaunches/doggoStyle.jpg",
+    para: "DoggoStyle is creating the greatest 3D VR card game ever! 8888 collection",
+    link: "https://www.mintdoggostyle.art/",
+    wlSale: "WL April 14th, 8:30pm UTC",
+    publicSale: "Public April 14th, 9pm UTC",
   },
 ];
